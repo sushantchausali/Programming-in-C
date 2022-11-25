2ff7cc13c8b1fe04620fa3776e1e61b2d93883f3
sushantchausali <117421975+sushantchausali@users.noreply.github.com>
2022-11-25 17:45:43 +0530

    Add files via upload

---
+++ Semantic error.c
@@ -0,0 +1,11 @@
+Use of a un-initialized variable.
+int i;
+i=i+2;
+o	Type compatibility
+int b = "javatpoint";
+o	Errors in expressions
+int a, b, c;
+a+b = c;
+o	Array index out of bound
+int a[10];
+a[10] = 34;
